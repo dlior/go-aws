2d2ce0eed5f44956937c3fd1de523ece3de8b8a0
Lior Degu <lior.degu@gmail.com>
2024-04-13 23:51:48 +0300

    test aws lambda function

---
+++ go-aws.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type GoAwsStackProps struct {
+	awscdk.StackProps
+}
+
+func NewGoAwsStack(scope constructs.Construct, id string, props *GoAwsStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	awslambda.NewFunction(stack, jsii.String("myLambdaFunction"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Code:    awslambda.S3Code_FromAsset(jsii.String("lambda/function.zip"), nil),
+		Handler: jsii.String("main"),
+	})
+
+	return stack
+}
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	NewGoAwsStack(app, "GoAwsStack", &GoAwsStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+func env() *awscdk.Environment {
+	return nil
+}
---
+++ lambda/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+func HandleEvent(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username cannot be empty")
+	}
+
+	return fmt.Sprintf("successfully called by - %s", event.Username), nil
+}
+
+func main() {
+	lambda.Start(HandleEvent)
+}
